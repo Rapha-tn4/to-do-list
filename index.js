81cfc7edcaf007f6b5b53b6b19ec9c4db41cdbe7
Rapha-tn4 <raphamello49@gmail.com>
2026-05-28 17:15:57 +0200

    erheth

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express"
+import dotenv from "dotenv"
+import { createClient } from "@supabase/supabase-js/dist/index.cjs"
+
 const app = express()
 const port = 3000
+
+
+//lit le fichier .env
+dotenv.config()
 
 //on utilise le moteur template ejs
 app.set("view engine" ,"ejs")
@@ -8,16 +15,40 @@ app.set("view engine" ,"ejs")
 //mes assets et mon style sont dans le dossier public
 app.use(express.static('public'))
 
+//bien traiter les donnes envoyees
+app.use(express.urlencoded({extended: true}))
+
+//base de donnée :
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 
-app.get('/', (req, res) => {
-    const user = {
-        nom: "DI readi",
-        prenom: "Ndio"
-    }
+app.get('/', async (req, res) => {
+    const { data, error } = await supabase
+    .from('Todo')
+    .select('*')
+
+    console.log(data)
   res.render("home.ejs" ,{"user" : user})
 })
+
+
+//route qui affiche le formulaire pour creer une tache
+app.get("/ajouter", (req,rep)=>{
+
+    rep.render("ajouter.ejs")
+})
+
+
+app.post("/ajouter",(req,res)=>{
+
+   //console.log(req.body)
+    await supabase.from('todo').insert(req.body)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
